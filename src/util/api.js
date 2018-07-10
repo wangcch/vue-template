959b468ab51fe339539d95b9e9c0768624c3be24
wangcch <ci713@qq.com>
2018-07-10 18:18:06 +0800

    update: api

--- src/util/api.js
+++ src/util/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 const qs = require('qs')
 const urlPrefix = '/api/'
-let api = {}
 
 const interceptors = () => {
   axios.interceptors.request.use((config) => {
@@ -17,102 +16,106 @@ const interceptors = () => {
   })
 }
 
-/**
- * GET api
- * @param {String} getfix url head
- * @param {Object} data parameters
- * @param {Object} self this
- */
-api.getUrlData = (getfix, data, callback, self) => {
-  const url = urlPrefix + getfix + (Object.keys(data).length > 0 ? `?${qs.stringify(data)}` : '')
-  interceptors()
-  axios.get(url)
-    .then(res => {
-      callback(res)
-    }).catch(error => {
-      if (error.response) {
-        callback(error.response)
-      } else {
-        callback(error, true)
-      }
-    })
-}
-
-/**
- * DELETE api
- * @param {String} getfix url head
- * @param {Object} data parameters
- * @param {Object} self this
- */
-api.deleteUrlData = (getfix, data, callback, self) => {
-  const url = urlPrefix + getfix + (Object.keys(data).length > 0 ? `?${qs.stringify(data)}` : '')
-  interceptors()
-  axios.delete(url)
-    .then(res => {
-      callback(res)
-    }).catch(error => {
-      if (error.response) {
-        callback(error.response)
-      } else {
-        callback(error, true)
-      }
-    })
-}
-
-/**
- * POST api
- * @param {String} postfix url head
- * @param {Object} data parameters
- * @param {Object} self this
- */
-api.postUrlData = (postfix, data, callback, isJson, self) => {
-  interceptors()
-  axios.post(urlPrefix + postfix, isJson ? data : qs.stringify(data))
-    .then(res => {
-      callback(res)
-    })
-    .catch(error => {
-      if (error.response) {
-        callback(error.response)
-      } else {
-        callback(error, true)
-      }
-    })
-}
-
-/**
- * PUT api
- * @param {String} postfix url head
- * @param {Object} data parameters
- * @param {Object} self this
- */
-api.putUrlData = (postfix, data, callback, isJson, self) => {
-  interceptors()
-  axios.put(urlPrefix + postfix, isJson ? data : qs.stringify(data))
-    .then(res => {
-      callback(res)
-    })
-    .catch(error => {
-      if (error.response) {
-        callback(error.response)
-      } else {
-        callback(error, true)
-      }
-    })
-}
-
-/**
- * GET Download
- * @param {String} url download url
- */
-api.downloadUrl = (url) => {
-  let iframe = document.createElement('iframe')
-  iframe.style.display = 'none'
-  iframe.src = url
-  iframe.onload = function () {
-    document.body.removeChild(iframe)
+class API {
+  /**
+   * GET api
+   * @param {String} getfix url head
+   * @param {Object} data parameters
+   * @param {Object} self this
+   */
+  getUrlData = (getfix, data, callback, self) => {
+    const url = `${urlPrefix}${getfix}${(Object.keys(data).length > 0 ? `?${qs.stringify(data)}` : '')}`
+    interceptors()
+    axios.get(url)
+      .then(res => {
+        callback(res)
+      }).catch(error => {
+        if (error.response) {
+          callback(error.response)
+        } else {
+          callback(error, true)
+        }
+      })
   }
-  document.body.appendChild(iframe)
+
+  /**
+   * DELETE api
+   * @param {String} getfix url head
+   * @param {Object} data parameters
+   * @param {Object} self this
+   */
+  deleteUrlData = (getfix, data, callback, self) => {
+    const url = `${urlPrefix}${getfix}${(Object.keys(data).length > 0 ? `?${qs.stringify(data)}` : '')}`
+    interceptors()
+    axios.delete(url)
+      .then(res => {
+        callback(res)
+      }).catch(error => {
+        if (error.response) {
+          callback(error.response)
+        } else {
+          callback(error, true)
+        }
+      })
+  }
+
+  /**
+   * POST api
+   * @param {String} postfix url head
+   * @param {Object} data parameters
+   * @param {Object} self this
+   */
+  postUrlData = (postfix, data, callback, isJson, self) => {
+    interceptors()
+    axios.post(`${urlPrefix}${postfix}`, isJson ? data : qs.stringify(data))
+      .then(res => {
+        callback(res)
+      })
+      .catch(error => {
+        if (error.response) {
+          callback(error.response)
+        } else {
+          callback(error, true)
+        }
+      })
+  }
+
+  /**
+   * PUT api
+   * @param {String} postfix url head
+   * @param {Object} data parameters
+   * @param {Object} self this
+   */
+  putUrlData = (postfix, data, callback, isJson, self) => {
+    interceptors()
+    axios.put(`${urlPrefix}${postfix}`, isJson ? data : qs.stringify(data))
+      .then(res => {
+        callback(res)
+      })
+      .catch(error => {
+        if (error.response) {
+          callback(error.response)
+        } else {
+          callback(error, true)
+        }
+      })
+  }
+
+  /**
+   * GET Download
+   * @param {String} url download url
+   */
+  downloadUrl = (url) => {
+    let iframe = document.createElement('iframe')
+    iframe.style.display = 'none'
+    iframe.src = url
+    iframe.onload = function () {
+      document.body.removeChild(iframe)
+    }
+    document.body.appendChild(iframe)
+  }
 }
+
+const api = new API()
 
 export default api
